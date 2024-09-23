dfa7e251603a13221c9b9df53337f4e6666f61bb
debabrata <debabrataswainofficial@gmail.com>
2024-09-24 00:14:41 +0530

    File is modified by Ethan

--- application.java
+++ application.java
@@ -3,7 +3,23 @@ class Main {
 
     int n = 10, firstTerm = 0, secondTerm = 1;
     System.out.println("Fibonacci Series till " + n + " terms:");
-
+#Feature201 is changed by Ethan in oct2023
+public class LinearSearchExample{    
+public static int linearSearch(int[] arr, int key){    
+        for(int i=0;i<arr.length;i++){    
+            if(arr[i] == key){    
+                return i;    
+            }    
+        }    
+        return -1;    
+    }    
+    public static void main(String a[]){    
+        int[] a1= {10,20,30,50,70,90};    
+        int key = 50;    
+        System.out.println(key+" is found at index: "+linearSearch(a1, key));    
+    }    
+}
+#The changes is added abo0ve in the program
     for (int i = 1; i <= n; ++i) {
       System.out.print(firstTerm + ", ");
 
